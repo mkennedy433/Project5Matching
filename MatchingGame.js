45ce4e29635ee1244803fd27bebd8c5aa93f9101
Alexander Collazo <alexander.collazo424@west-mec.org>
2016-11-28 15:05:49 -0700

    Update MatchingGame.js

--- MatchingGame.js
+++ MatchingGame.js
@@ -1,2 +1,39 @@
-/* Created by alexander.collazo424 on 11/18/16. */
+var numberOfFaces = 5;
+var theLeftSide = document.getElementById("leftSide");
+var theRightSide = document.getElementById("rightSide");
+var theBody = document.getElementsByTagName("body")[0];
+
+function generateFaces() {
+    while (theLeftSide.firstChild) {
+        theLeftSide.removeChild(theLeftSide.firstChild)
+    }
+    while (theRightSide.firstChild) {
+        theRightSide.removeChild(theRightSide.firstChild)
+    }
+    for (i = 0; i < numberOfFaces; ++ i) {
+        elem_img = document.createElement("img");
+        elem_img.src = "image/tumulty.png";
+        elem_img.style.top = Math.floor(Math.random() * 401) + "px";
+        elem_img.style.left = Math.floor(Math.random() * 401) + "px";
+        theLeftSide.appendChild(elem_img);
+    }
+
+    leftSideImages = theLeftSide.cloneNode(true);
+    leftSideImages.removeChild(leftSideImages.lastChild);
+    theRightSide.appendChild(leftSideImages);
+
+    theLeftSide.lastChild.onclick =
+        function nextLevel(event){
+            event.stopPropagation();
+            numberOfFaces += 5;
+            generateFaces();
+        }
+}
+generateFaces();
+
+theBody.onclick = function gameOver() {
+    alert("Game Over!");
+    theBody.onclick = null;
+    theLeftSide.lastChild.onclick = null;
+};
 
